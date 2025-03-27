49b1706078cb3d246dfba8e778adb9c468608ecc
AndySequeira <andygarci357@gmail.com>
2025-03-27 14:01:56 -0600

    Models y context
    Models y context

---
+++ EdilitSoft/EdilitSoft.app/Models/Categorias.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EdilitSoft.app.Models
+{
+    public class Categorias
+    {
+        public int IdCategoria { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio"), MaxLength(100, ErrorMessage = "No más de 100 carácteres")]
+        public string NombreCategoria { get; set; }
+        public bool Activo { get; set; }
+
+        //Relacion con libros
+        public IEnumerable<Libros> Libros { get; set; }
+    }
+}
