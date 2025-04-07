eee0770c94d8a541d15e21c45af47ecb45c92d4a
DanGonzaG <dan.g.g@hotmail.es>
2025-04-07 16:46:38 -0600

    se agrega validaciones de en ApiBook

--- EdilitSoft/EdilitSoft.app/obj/Debug/net8.0/EdilitSoft.app.AssemblyInfo.cs
+++ EdilitSoft/EdilitSoft.app/obj/Debug/net8.0/EdilitSoft.app.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EdilitSoft.app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be1603889021ba27c8a8523d587cdec12d96dd47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+855ffba7ff3d33c236a78b7f47e56e666046046f")]
 [assembly: System.Reflection.AssemblyProductAttribute("EdilitSoft.app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EdilitSoft.app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
